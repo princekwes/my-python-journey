8376795782bb8e5e5a5e66235b716714cbb93f0c
Prince Amponsah <princekwes@gmail.com>
2023-10-28 20:12:12 +0000

    Update guess_game.py

--- guess_game.py
+++ guess_game.py
@@ -1,9 +1,16 @@
 secret_word = "giraffe"
 guess = ""
 count = 0
-while guess != secret_word:
-    guess = input( " Enter a secred word: \n")
-    
-    if guess ==  secret_word:
+count_limit = 3
+out_of_guesses = False
+while guess != secret_word and not(out_of_guesses):
+    if count < count_limit:
+         guess = input( " Enter a secred word: \n")
+         count += 1
+    else:
+        out_of_guesses = True
+if  out_of_guesses:
+        print( " Out  Of  Guesses, You Lose!")
+else:
         print( " Got You! You Got it.")
    
